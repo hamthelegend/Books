9e9ba4aca1c80b6091f3cc41ce0aee8c51b53a3d
hamthelegend <hamuelxd@gmail.com>
2023-11-26 21:11:58 +0800

    Increased version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thebrownfoxx.books"
         minSdk = 27
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
